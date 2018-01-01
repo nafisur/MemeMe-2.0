15668106244a608b4ebf0dec77061157deb41934
Nafisur Ahmed <cse.nafisur@gmail.com>
2018-01-01 22:32:04 +0530

    Changes

---
+++ MemeMe 2.0/CustomCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CustomCollectionViewCell.swift
+//  MemeMe 2.0
+//
+//  Created by Nafisur Ahmed on 01/01/18.
+//  Copyright © 2018 Nafisur Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imgView: UIImageView!
+}
--- MemeMe 2.0/MemeCollectionVC.swift
+++ MemeMe 2.0/MemeCollectionVC.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class MemeCollectionVC: UIViewController {
+class MemeCollectionVC: UIViewController,UICollectionViewDelegate, UICollectionViewDataSource {
 
+     var memes = [Meme]()
+    @IBOutlet weak var memeCollectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        memeCollectionView.delegate = self
+        memeCollectionView.dataSource = self
 
       
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        memes = appDelegate.memes
+        memeCollectionView.reloadData()
+    }
 
-  
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return memes.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = memeCollectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CustomCollectionViewCell
+        
+        cell.imgView.image = memes[indexPath.row].memedImage
+        return cell
+    }
 }
